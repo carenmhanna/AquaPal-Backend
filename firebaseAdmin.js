6cb5a7c5c43dc3de397b7f15826f7b3830e6ca8b
Caren Mhanna <carenmhanna2024@gmail.com>
2025-11-22 22:48:24 +0200

    UPDATE FINALE

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,10 +2,17 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Check if the env variable exists (Render)
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : require("./firebase-service-account.json"); // local fallback
+
+// Initialize Firebase Admin
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
