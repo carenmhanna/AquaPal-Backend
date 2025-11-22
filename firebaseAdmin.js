2051f83ede31a728e54f7810725c48acaf57cdb8
Caren Mhanna <carenmhanna2024@gmail.com>
2025-11-22 23:00:29 +0200

    UPDATE FINALEEE

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -17,6 +17,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("✅ Firebase Admin initialized successfully");
 }
 
 export default admin;
